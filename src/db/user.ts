7764a1a108c72db94b3428c36cda791bc5c67541
Michael Romashov <mromashov@icloud.com>
2025-04-29 21:28:41 -0400

    Set up basic user auth

---
+++ src/db/user.ts
@@ -0,0 +1,87 @@
+import argon2, { type Options as ArgonOptions } from "argon2";
+import type { ResultSetHeader, RowDataPacket } from "mysql2";
+
+import { pool } from "@/db/index";
+
+// As per OWASP recommendations:
+// https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+const ARGON_OPTIONS: ArgonOptions = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 2,
+    parallelism: 1,
+    hashLength: 32,
+    secret: process.env.SECRET_KEY ? Buffer.from(process.env.SECRET_KEY) : undefined,
+};
+
+interface UserRow extends RowDataPacket {
+    id: number;
+    email: string;
+    password_hash: string;
+    first_name: string;
+    last_name: string;
+}
+
+export type User = Pick<UserRow, "id" | "email" | "first_name" | "last_name">;
+
+/**
+ * Attempt to retrieve user with given password and password.
+ *
+ * @returns `User` struct if the combination exists, `null` if not
+ */
+export async function getUser(email: string, password: string): Promise<User | null> {
+    const [res] = await pool.execute<UserRow[]>(
+        `SELECT
+            id,
+            email,
+            password_hash,
+            first_name,
+            last_name,
+        FROM user
+        WHERE user.email = :email`,
+        { email },
+    );
+
+    if (res.length !== 1) {
+        return null;
+    }
+
+    const user = res[0];
+
+    if (await argon2.verify(user.password_hash, password, ARGON_OPTIONS)) {
+        return {
+            id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+        };
+    }
+
+    return null;
+}
+
+/**
+ * Creates a user with the given parameters.
+ *
+ * @returns `id` of newly created user
+ */
+export async function createUser(
+    email: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+): Promise<number | null> {
+    const hash = await argon2.hash(password, ARGON_OPTIONS);
+
+    try {
+        const [res] = await pool.execute<ResultSetHeader>(
+            `INSERT INTO user (first_name, last_name, email, password_hash)
+            VALUES (:first_name, :last_name, :email, :hash)`,
+            { first_name, last_name, email, hash },
+        );
+
+        return res.insertId;
+    } catch (e) {
+        return null;
+    }
+}
---
+++ src/utils/cookies.ts
@@ -0,0 +1,49 @@
+import { createCipheriv, createDecipheriv } from "node:crypto";
+import { cookies } from "next/headers";
+
+import type { User } from "@/db/user";
+
+const ALGO = "aes-256-cbc";
+
+export interface AuthCookie {
+    iat: number;
+    exp: number;
+    user: User;
+}
+
+export function encryptCookie(user: User): string {
+    const payload: AuthCookie = {
+        iat: Math.floor(new Date().getTime() / 1000),
+        exp: 60 * 60 * 24,
+        user,
+    };
+
+    const cipher = createCipheriv(ALGO, process.env.SECRET_KEY, process.env.SECRET_IV);
+    return cipher.update(JSON.stringify(payload), "utf-8", "base64") + cipher.final("base64");
+}
+
+export function decryptCookie(cookie: string): User | null {
+    const decipher = createDecipheriv(ALGO, process.env.SECRET_KEY, process.env.SECRET_IV);
+    const decrypted = decipher.update(cookie, "base64", "utf-8") + decipher.final("utf-8");
+
+    const payload: AuthCookie = JSON.parse(decrypted);
+    if (payload.iat + payload.exp < Math.floor(new Date().getTime() / 1000)) {
+        return null;
+    }
+
+    return payload.user;
+}
+
+export function getCurrentUser(): User | null {
+    const sessionCookie = cookies()?.get("session")?.value;
+    if (!sessionCookie) {
+        return null;
+    }
+
+    const decryptedUser = decryptCookie(sessionCookie);
+    if (!decryptedUser || typeof decryptedUser.id !== "number") {
+        return null;
+    }
+
+    return decryptedUser;
+}
